e318b094f1410955bc413075916c78bb450d3912
Mahendra Kola <mahendra.kola@daimler.com>
2019-10-31 09:10:30 +0530

    first commit

---
+++ src/main/java/com/chat/BootChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootChatAppApplication.class, args);
+	}
+
+}
